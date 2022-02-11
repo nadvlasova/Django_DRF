5fcd27c70d922b8361ebb21613aca35bf88c7d32
Nadezhda <nadvlasova@rambler.ru>
2022-02-11 20:23:56 +0800

    task 3 projectapp

--- to_do/project/serializers.py
+++ to_do/project/serializers.py
@@ -7,11 +7,13 @@ class ProjectModelSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = Project
-        fields = '__all__'
+        # fields = '__all__'
+        fields = 'first_name'
 
 
 class TODOModelSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = TODO
-        fields = '__all__'
+        # fields = '__all__'
+        fields = 'last_name'

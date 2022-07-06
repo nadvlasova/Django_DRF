c6232e646296fec6f29536a606b25c1540958148
Nadezhda <45838008+nadvlasova@users.noreply.github.com>
2022-07-06 15:58:35 +0800

    Update settings.py

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xh(u&eauf9s1h27y6*nzapb6!+7umv9%8cvc+(yb3)@-j%3dk!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -98,16 +98,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'db',
-#         'USER': 'nadezhda',
-#         'PASSWORD': '1',
-#         'HOST': 'db',
-#         'PORT': '5432'
-#     }
-# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
